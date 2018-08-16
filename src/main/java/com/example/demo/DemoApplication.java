130a4ae9f6c70f10a5b6d59bfc3bcfa8312949b1
itziklavon <34890672+itziklavon@users.noreply.github.com>
2018-08-16 18:39:53 +0300

    Add files via upload

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,28 @@
 package com.example.demo;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-
-        new SpringApplicationBuilder(DemoApplication.class)
+        ConfigurableApplicationContext configurableApplicationContext = new SpringApplicationBuilder(DemoApplication.class)
                 .properties("spring.config.name:application",
                         "spring.config.location:/opt/conf/application.properties")
                 .build()
                 .run(args);
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        context.reset();
+        Logger root = (Logger) LoggerFactory.getLogger(DemoApplication.class);
+        String logLevel = configurableApplicationContext.getEnvironment().getProperty("LOGGING_LEVEL");
+        root.setLevel(Level.toLevel(logLevel, Level.ERROR));
     }
 
 }
